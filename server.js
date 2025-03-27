3586f36f0276cada20dfb29ccf66b58d9a5da107
Rob Cl <rclandrover@gmail.com>
2025-03-27 12:49:22 +0000

    Update server.js

--- server.js
+++ server.js
@@ -7,18 +7,15 @@ const Readline = require('@serialport/parser-readline');
 const fs = require('fs');
 const { networkInterfaces } = require('os');
 const i2c = require('i2c-bus');
-
-const ADXL345 = require('./s_adxl345.js');
-const ITG3205 = require('./s_itg3205.js');
-const QMC5883L = require('./s_qmc5883l.js');
+const ADXL345 = require('adxl345-sensor');
+const ITG3205 = require('itg3205-sensor');
+const QMC5883L = require('qmc5883l-sensor');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 const port = 3000;
-
-const Sbaud = 115200;
 
 // Automatically detect the Raspberry Pi's IP
 function getIPAddress() {
@@ -39,7 +36,7 @@ const ipAddress = getIPAddress();
 app.use(express.static(__dirname + '/public'));
 
 // Serial port setup for UM980 GNSS receiver
-const gpsPort = new SerialPort('/dev/serial0', { baudRate: Sbaud });
+const gpsPort = new SerialPort('/dev/serial0', { baudRate: 115200 });
 const gpsParser = gpsPort.pipe(new Readline({ delimiter: '\n' }));
 
 // I2C setup for sensors
@@ -71,34 +68,12 @@ gpsParser.on('data', (data) => {
     }
 });
 
-//setInterval(() => {
-//    const data = accelerometer.readAcceleration();
-//    console.log(`X: ${data.x.toFixed(3)}g, Y: ${data.y.toFixed(3)}g, Z: ${data.z.toFixed(3)}g`);
-//}, 500);
-
-//setInterval(() => {
-//    const data = magnetometer.readGauss();
-//    console.log(`Magnetic Field: X=${data.x.toFixed(2)} Y=${data.y.toFixed(2)} Z=${data.z.toFixed(2)} (Gauss)`);
-//}, 1000);
-
-//setInterval(() => {
-//    console.log("Gyro (°/s):", gyroscope.readGyroDPS());
-//    console.log("Temp (°C):", gyroscope.readTemperature().toFixed(2));
-//}, 1000);
-
-// Close the I2C connection when done
-//process.on('SIGINT', () => {
-//    magnetometer.close();
-//    console.log('HMC5883L sensor connection closed.');
-//    process.exit();
-//});
-
 // Read sensor data and emit to client
 async function readSensors() {
     try {
-        const accel = await accelerometer.getAcceleration();
-        const gyro = await gyroscope.getRotation();
-        const mag = await magnetometer.getHeading();
+        const accel = await accelerometer.getAcceleration().catch(err => ({ error: err.message }));
+        const gyro = await gyroscope.getRotation().catch(err => ({ error: err.message }));
+        const mag = await magnetometer.getHeading().catch(err => ({ error: err.message }));
 
         io.emit('sensorData', { accel, gyro, mag });
     } catch (error) {
